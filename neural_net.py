ef3f5a0536de0285c7fb218aedbdda3ab885bb41
Aiden Mizhen <aiden@boriska.com>
2023-09-05 16:42:15 -0400

    Started outline

---
+++ neural_net.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+class NN:
+    def __init__(self):
+        self.layers = None  # [np.array, ....]
+        self.consts = None
+        self.actives = None
+        pass
+
+    def compute(self, given):
+        pass
+
+    def train(self, train_set, step, iters):
+        pass
+
+    def cost(self):
+        pass
+
+    def sigmoid(self):
+        pass
+
+    def relu(self):
+        pass
+
+    def tanh(self):  # ALREADY DEFINED
+        pass
