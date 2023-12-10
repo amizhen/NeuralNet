fd0884f60119f4ee5aa85af338888b7804f3cccf
Aiden Mizhen <aiden@boriska.com>
2023-12-09 19:50:16 -0500

    need to review ml stuff

--- neural_net.py
+++ neural_net.py
@@ -13,18 +13,22 @@ class NN:
         self.layers = []  # [np.array, ....]
         self.consts = []
         self.activ_funcs = []
+        self.cache = []
         pass
 
-    def forward_prop(self, given, cache=False):
+    def forward_prop(self, given, cache=False): # TODO Change to not use so much memory
         c = given ## ???
         for i in range(len(self.layers)):
-            c = c @ self.layers[i]
-            c = c + self.consts[i]
+            a = np.copy(c)
+            c = c @ self.layers[i] + self.consts[i]
+            z = np.copy(c)
             c = self.activ_funcs[i](c)
+            if cache:
+                self.cache.append((z, a))
         return c
 
     def init_params(self, shape, activ_funcs):
-        # shape is of format [<num params> <l1_size> <l2_size> ... <output_size>]
+        # shape is of format [<num params>, <l1_size>, <l2_size>, ... <output_size>]
         table = {'lin': self.lin, 'relu': self.relu, 'sigmoid': self.sigmoid, 'tanh': self.tanh}
         for i in range(len(shape)-1):
             self.layers.append(np.random.randn(shape[i], shape[i+1])*0.01)
@@ -34,17 +38,40 @@ class NN:
     def load_params(self, fd):
         pass
 
-    def train(self, train_set, step, iters):
+
+    def train(self, train_data, actual, step=0.00001, passes=100000):
+        for i in range(passes):
+            self.train_pass(train_data, actual, step=step)
+
+    def train_pass(self, train_data, actual, step=0.00001):
+        result = self.forward_prop(train_data)
+        dA = - (np.divide(actual, result) - np.divide(1 - actual, 1 - result))
+
+        self.cache = []
+        for i in range(len(self.layers), 0, -1):
+            dW, db, dA = self.one_deriv(i, dA)
+            self.layers[i] *= [1-dW*step]
+            self.consts[i] *= [1-db*step]
+
+
+    def one_deriv(self, i, dA):
+        table = {self.relu: self.relu_grad, self.sigmoid: self.sigmoid_grad} ## ADD TANH
+        g = table[self.activ_funcs[i]]
+        dZ = dA * g(self.cache[i][0])
+        # d_const =
+        m = self.cache[-1][1].shape[0]
+        dW = 1/m * self.cache[i-1][1] @ dZ.T
+        db = 1/m * dZ.sum(axis=0, keepdims=True)
+        dA = dZ.T @ self.layers[i]
+        return dW, db, dA
+
+
+    def mse(self, actual, result):
         pass
 
-    # def one_deriv(self, i, cache):
-    #     table = {self.relu: self.relu_grad, self.sigmoid: self.sigmoid_grad} ## ADD TANH
-    #     g = table[self.activ_funcs[i]]
-    #     d_const =
-
-    def cross_entropy(self, result, actual):
+    def cross_entropy(self, actual, result):
         # Y is number_examples X 1
-        return -1/result.shape[0]*np.sum(result*np.log(actual) + (1-result)*np.log(1-actual))
+        return -1/actual.shape[0]*np.sum(actual * np.log(result) + (1 - actual) * np.log(1 - result))
 
     def sigmoid(self, x):
         pass
